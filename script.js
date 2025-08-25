6db58e7195d50a36c4eb440fd311234bf27564bc
Dalton McMullen <thecodingegg@gmail.com>
2025-08-24 18:19:35 -0700

    Added functionality to store first number
    Started the true logic of the program. Using else statements currently.
       1. Added whatever was pressed to the display
       2. Stored the first number if an operator is pressed
    
    Next steps are to store operator and figure out how to store second
    number. Once that is completed, I can then evaluate the numbers that are
    stored with their respective operator.

--- script.js
+++ script.js
@@ -14,28 +14,66 @@ function divide(x, y) {
   return x / y;
 }
 
-const display = document.querySelector(".display");
+let display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const pressedButton = document.querySelectorAll("button");
+let currentDisplay = "";
 
-let firstNumber = "";
-numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    firstNumber = parseInt(number.innerText);
-    display.innerText = firstNumber;
+function buttonPress() {
+  displayLastPress(currentDisplay);
+  pressedButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.classList.contains("number")) {
+        currentDisplay += button.innerText;
+        displayLastPress(currentDisplay);
+      } else if (button.classList.contains("operator")) {
+        getFirstNumber();
+
+        currentDisplay = "";
+        currentDisplay += button.innerText;
+        displayLastPress(currentDisplay);
+      }
+    });
   });
-});
+}
 
-let secondNumber = "";
-numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    secondNumber = parseInt(number.innerText);
-    console.log(secondNumber);
+buttonPress();
+
+function getFirstNumber() {
+  let firstNumber = parseInt(currentDisplay);
+  if (isNaN(firstNumber)) {
+    console.log("Please enter a valid number");
+  } else {
+    console.log(firstNumber);
+    return firstNumber;
+  }
+}
+
+function getSecondNumber() {
+  let secondNumber = "";
+  numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+      secondNumber = parseInt(number.innerText);
+      return secondNumber;
+    });
   });
-});
+}
 
-let operator = "";
+// function getOperator() {
+//   currentDisplay += operators.innerText;
+//   displayLastPress(currentDisplay);
+// }
+
+function displayLastPress(content) {
+  display.innerText = content;
+}
 
 function operate(firstNumber, secondNumber, operator) {
+  firstNumber = getFirstNumber();
+  secondNumber = getSecondNumber();
+  // operator = getOperator();
+
   switch (operator) {
     case "+":
       return add(firstNumber, secondNumber);
